b7575260e0eab635c3b3a66760714c22db557174
Rui Zhao <christianoray@gmail.com>
2021-01-26 14:09:06 +0800

    add new app

---
+++ flutter_wanandroid-master/flutter_wanandroid-master/android/app/src/main/java/com/thl/flutterwanandroid/MainActivity.java
@@ -0,0 +1,7 @@
+package com.thl.flutterwanandroid;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
+
---
+++ flutter_wanandroid-master/flutter_wanandroid-master/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=E:\Flutter_SDK\flutter"
+export "FLUTTER_APPLICATION_PATH=E:\BIM_App\BIM_App\flutter_wanandroid-master\flutter_wanandroid-master"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_BUILD_NAME=0.2.6"
+export "FLUTTER_BUILD_NUMBER=26"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
